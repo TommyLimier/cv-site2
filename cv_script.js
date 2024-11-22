6efa8831ec46c952b7f2132d312b4bb30736096f
TommyLimier <tommy.limier@gmail.com>
2024-11-22 15:13:21 -0400

    Update cv_script.js

--- cv_script.js
+++ cv_script.js
@@ -1,13 +1,21 @@
-    window.addEventListener('DOMContentLoaded', () => {
-        const stickyHeader = document.getElementById('sticky-header');
-        const headerOffsetTop = stickyHeader.offsetTop; // Position initiale
+window.addEventListener('DOMContentLoaded', () => {
+    const stickyHeader = document.getElementById('sticky-header');
+    const headerOffsetTop = stickyHeader.offsetTop; 
 
-        window.addEventListener('scroll', () => {
-            if (window.scrollY >= headerOffsetTop) {
-                stickyHeader.classList.add('sticky'); // Ajoute la classe sticky
-            } else {
-                stickyHeader.classList.remove('sticky'); // Supprime la classe sticky
-            }
-        });
+    window.addEventListener('scroll', () => {
+        if (window.scrollY >= headerOffsetTop) {
+            stickyHeader.classList.add('sticky'); 
+        } else {
+            stickyHeader.classList.remove('sticky'); 
+        }
     });
+});
 
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerMenu = document.getElementById('burger-menu');
+    const navList = document.querySelector('.nav-list');
+
+    burgerMenu.addEventListener('click', () => {
+        navList.classList.toggle('visible');
+    });
+});
