6d10d7f85f3daf2b3656907e46e7eac28ad436fc
TommyLimier <tommy.limier@gmail.com>
2024-11-24 06:15:18 -0400

    Update cv_script.js

--- cv_script.js
+++ cv_script.js
@@ -1,21 +1,24 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const stickyHeader = document.getElementById('sticky-header');
-    const headerOffsetTop = stickyHeader.offsetTop; 
-
-    window.addEventListener('scroll', () => {
-        if (window.scrollY >= headerOffsetTop) {
-            stickyHeader.classList.add('sticky'); 
-        } else {
-            stickyHeader.classList.remove('sticky'); 
-        }
-    });
-});
-
 document.addEventListener('DOMContentLoaded', () => {
+    const stickyHeader = document.getElementById('sticky-header');
     const burgerMenu = document.getElementById('burger-menu');
     const navList = document.querySelector('.nav-list');
+    const backToTop = document.getElementById('backToTop');
 
+    // Sticky Header
+    const headerOffsetTop = stickyHeader.offsetTop;
+    window.addEventListener('scroll', () => {
+        stickyHeader.classList.toggle('sticky', window.scrollY >= headerOffsetTop);
+    });
+
+    // Menu burger
     burgerMenu.addEventListener('click', () => {
         navList.classList.toggle('visible');
+        navList.classList.toggle('hidden');
+    });
+
+    // Retour en haut
+    backToTop.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 });
